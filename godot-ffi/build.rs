f7eca93f04c8125d4fc29bae5e3e7ae6d925fbd1
Jan Haller <bromeon@gmail.com>
2023-10-14 09:48:41 +0200

    (undo) even more minimal godot-ffi/build.rs

--- godot-ffi/build.rs
+++ godot-ffi/build.rs
@@ -4,46 +4,16 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-// See also prebuilt's generator/build.rs which is similar in nature.
-
-// use std::path::Path;
-
 use std::env;
 use std::process::Command;
 
 fn main() {
-    // let mut watch = godot_bindings::StopWatch::start();
-
-    // It would be better to generate this in /.generated or /target/godot-gen, however IDEs currently
-    // struggle with static analysis when symbols are outside the crate directory (April 2023).
-    // let gen_path = Path::new(concat!(env!("CARGO_MANIFEST_DIR"), "/src/gen"));
-
-    // C header is not strictly required, however it is generated for debugging, and to allow CI
-    // to check for differences (tweak.patch).
-    // let h_path = gen_path.join("gdextension_interface.h");
-    // let rs_path = gen_path.join("gdextension_interface.rs");
-
-    // godot_bindings::clear_dir(gen_path, &mut watch);
-    // godot_bindings::write_gdextension_headers(&h_path, &rs_path, &mut watch);
-
-    //godot_codegen::generate_sys_files(gen_path, &h_path, &mut watch);
-
-    // watch.write_stats_to(&gen_path.join("ffi-stats.txt"));
-    //println!("cargo:rerun-if-changed=build.rs");
-
-    // godot_bindings::emit_godot_version_cfg();
-
     let godot_bin = env::var("GODOT4_BIN").expect("env var 'GODOT4_BIN' not found");
 
     let output = Command::new(godot_bin.clone())
         .arg("--version")
         .output()
-        .unwrap_or_else(|_| {
-            panic!(
-                "failed to invoke Godot executable '{}'",
-                godot_bin
-            )
-        });
+        .unwrap_or_else(|_| panic!("failed to invoke Godot executable '{}'", godot_bin));
 
     if !output.status.success() {
         panic!("failed to read Godot version from {}", godot_bin);
